d8e767fcdf3f9f7694c4cec4b8e887777183b23e
djkloop <1044032256@qq.com>
2019-03-06 16:13:12 +0800

    update

--- generator/tools/appic.js
+++ generator/tools/appic.js
@@ -8,13 +8,16 @@ function addDependencies(api) {
 }
 
 function renderFiles(api, opts) {
-    const fs = require('fs');
-    const files = {
-        './src/App.vue': '../templates/default/src/App.vue',
-        './src/views/manager/tool/app/index.vue': '../templates/default/src/App.vue',
-    }
-    api.render(files, opts);
+
+    // 把默认需要的东西准备好...
     api.render('../templates/default')
+
+    // 路由相关的
+
+    // 在添加一些配置文件
+    api.render({
+        './.editorconfig': '../templates/default/_editorconfig'
+    })
 }
 
 module.exports = {
