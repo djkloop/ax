2d58621df6fb255257e52b6729a28e9b2e6033e1
djkloop <1044032256@qq.com>
2019-03-06 12:43:38 +0800

    update

--- generator/index.js
+++ generator/index.js
@@ -1,3 +1,7 @@
 module.exports = (api, opts, rootOptions) => {
-    console.log(api, opts, rootOptions)
+    console.log('api', api)
+    console.log('--------------------牛逼的分割线------------------');
+    console.log('opts', opts)
+    console.log('--------------------牛逼的分割线------------------');
+    console.log('rootOptions', rootOptions)
 }
