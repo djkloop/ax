13a1801b94cec0d13564a48463006a8b81183105
djkloop <1044032256@qq.com>
2019-03-06 15:44:15 +0800

    update

--- generator/templates/default/src/App.vue
+++ generator/templates/default/src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div class="good">
-    爽啊...
+  <div
+    id="main"
+    class="app-main"
+  >
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+/* 默认转换首页  */
 export default {
+  data () {
+    return {
+      theme: this.$store.state.app.themeColor
+    };
+  },
+  mounted () {
 
-}
+  },
+  beforeDestroy () {
+
+  },
+  methods: {
+
+  }
+};
 </script>
 
 <style>
-
+html,
+body {
+  width: 100%;
+  height: 100%;
+  background: #f0f0f0;
+  overflow: hidden;
+}
+.app-main {
+  width: 100%;
+  height: 100%;
+}
 </style>
